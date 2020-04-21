d7ae4bd54bdcb968cf98d2c2cb2871e5e245a789
Andrey Mukamolov <amv@bamboogroup.eu>
2020-04-21 14:12:46 +0300

    Added view binding

--- app/src/main/java/io/github/fobo66/factcheckerassistant/ui/main/MainFragment.kt
+++ app/src/main/java/io/github/fobo66/factcheckerassistant/ui/main/MainFragment.kt
@@ -1,11 +1,32 @@
 package io.github.fobo66.factcheckerassistant.ui.main
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import io.github.fobo66.factcheckerassistant.R
+import io.github.fobo66.factcheckerassistant.databinding.MainFragmentBinding
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class MainFragment : Fragment(R.layout.main_fragment) {
 
     private val mainViewModel: MainViewModel by viewModel()
 
+    private var _binding: MainFragmentBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = MainFragmentBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
 }
