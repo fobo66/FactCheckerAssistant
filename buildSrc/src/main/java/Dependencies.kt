8a7a9285023ef5844bf82588100815462493e8cb
Andrey Mukamolov <fobo66@protonmail.com>
2020-04-21 21:01:19 +0300

    Further buildSrc replacement, WIP

--- buildSrc/src/main/java/Dependencies.kt
+++ buildSrc/src/main/java/Dependencies.kt
@@ -1,6 +1,3 @@
-import java.io.File
-import java.util.Properties
-
 /*
  *    Copyright 2020 Andrey Mukamolov
  *
@@ -18,10 +15,18 @@ import java.util.Properties
  */
 
 object Versions {
-  const val kotlin = "1.3.72"
-  const val koin = "2.1.5"
+    const val kotlin = "1.3.72"
+    const val koin = "2.1.5"
+    const val moshi = "1.9.2"
+    const val retrofit = "2.8.1"
 }
 
 object Dependencies {
-  val kotlinStandardLibrary = "org.jetbrains.kotlin:kotlin-stdlib-jdk8:${Versions.kotlin}"
+    val kotlinStandardLibrary = "org.jetbrains.kotlin:kotlin-stdlib-jdk8:${Versions.kotlin}"
+    val moshi = "com.squareup.moshi:moshi:${Versions.moshi}"
+    val moshiCodegen = "com.squareup.moshi:moshi-kotlin-codegen:${Versions.moshi}"
+    val koinAndroid = "org.koin:koin-android:${Versions.koin}"
+    val koinScope = "org.koin:koin-androidx-scope:${Versions.koin}"
+    val koinViewModel = "org.koin:koin-androidx-viewmodel:${Versions.koin}"
+    val koinFragment = "org.koin:koin-androidx-fragment:${Versions.koin}"
 }
