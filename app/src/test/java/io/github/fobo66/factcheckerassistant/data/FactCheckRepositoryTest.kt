62e46a78d77b5d0f6d3ed9928f6e65ade49a143a
Andrey Mukamolov <amv@bamboogroup.eu>
2020-04-30 16:51:29 +0300

    Refactored test to use MockRetrofit

--- app/src/test/java/io/github/fobo66/factcheckerassistant/data/FactCheckRepositoryTest.kt
+++ app/src/test/java/io/github/fobo66/factcheckerassistant/data/FactCheckRepositoryTest.kt
@@ -1,44 +1,39 @@
 package io.github.fobo66.factcheckerassistant.data
 
 import io.github.fobo66.factcheckerassistant.api.FactCheckerApi
+import io.github.fobo66.factcheckerassistant.api.models.FactCheckResponse
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.runBlocking
-import okhttp3.mockwebserver.MockResponse
-import okhttp3.mockwebserver.MockWebServer
 import org.junit.Before
 import org.junit.Test
 import retrofit2.Retrofit
-import retrofit2.converter.moshi.MoshiConverterFactory
+import retrofit2.mock.MockRetrofit
 
 @ExperimentalCoroutinesApi
 class FactCheckRepositoryTest {
     private lateinit var factCheckRepository: FactCheckRepository
-    private lateinit var mockWebServer: MockWebServer
 
     @Before
     fun setUp() {
-        mockWebServer = MockWebServer()
 
-        val factCheckerApi = Retrofit.Builder()
-            .baseUrl(mockWebServer.url("/"))
-            .addConverterFactory(MoshiConverterFactory.create())
+        val retrofit = Retrofit.Builder()
+            .baseUrl("http://example.com")
             .build()
-            .create(FactCheckerApi::class.java)
 
-        factCheckRepository = FactCheckRepository(factCheckerApi)
+        val delegate = MockRetrofit.Builder(retrofit)
+            .build().create(FactCheckerApi::class.java)
+
+        factCheckRepository = FactCheckRepository(
+            delegate.returningResponse(
+                FactCheckResponse(
+                    listOf(), null
+                )
+            )
+        )
     }
 
     @Test
     fun `Search for claim and get no results`() {
-        mockWebServer.enqueue(
-            MockResponse().setBody(
-                """
-            {
-                "claims": []
-            }
-            """.trimIndent()
-            )
-        )
 
         runBlocking {
             factCheckRepository.search("test", this)
