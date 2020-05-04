63d7de2673554a4525a19be03dd5424746ad073a
Andrey Mukamolov <fobo66@protonmail.com>
2020-05-04 22:55:35 +0300

    Fixed API failures due to null values

--- app/src/main/java/io/github/fobo66/factcheckerassistant/api/models/Claim.kt
+++ app/src/main/java/io/github/fobo66/factcheckerassistant/api/models/Claim.kt
@@ -5,7 +5,7 @@ import com.squareup.moshi.JsonClass
 @JsonClass(generateAdapter = true)
 data class Claim(
     val text: String,
-    val claimant: String,
-    val claimDate: String,
+    val claimant: String?,
+    val claimDate: String?,
     val claimReview: List<ClaimReview>
 )
