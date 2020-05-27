a8dd4980d9c9a140a647b546092a441a5dd557e8
Andrey Mukamolov <amv@bamboogroup.eu>
2020-05-27 11:26:02 +0300

    Updated deps

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ buildscript {
         jcenter()
     }
     dependencies {
-        classpath("com.android.tools.build:gradle:4.1.0-alpha09")
+        classpath("com.android.tools.build:gradle:4.1.0-alpha10")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:${Versions.kotlin}")
     }
 }
