2f49c20111fe66bae9c741f7a5b9d1d17d812d02
Andrey Mukamolov <fobo66@protonmail.com>
2020-06-20 11:51:04 +0300

    Added stubs for progress displaying in adapter

---
+++ app/src/main/java/io/github/fobo66/factcheckerassistant/ui/list/ClaimsProgressAdapter.kt
@@ -0,0 +1,34 @@
+package io.github.fobo66.factcheckerassistant.ui.list
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.paging.LoadState
+import androidx.paging.LoadStateAdapter
+import androidx.recyclerview.widget.RecyclerView
+import io.github.fobo66.factcheckerassistant.databinding.ClaimsLoadingProgressHeaderBinding
+
+class ClaimsProgressAdapter : LoadStateAdapter<ClaimsProgressViewHolder>() {
+    override fun onBindViewHolder(holder: ClaimsProgressViewHolder, loadState: LoadState) {
+        holder.bind(loadState)
+    }
+
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        loadState: LoadState
+    ): ClaimsProgressViewHolder {
+        return ClaimsProgressViewHolder(
+            ClaimsLoadingProgressHeaderBinding.inflate(
+                LayoutInflater.from(
+                    parent.context
+                ), parent, false
+            )
+        )
+    }
+}
+
+class ClaimsProgressViewHolder(private val binding: ClaimsLoadingProgressHeaderBinding) :
+    RecyclerView.ViewHolder(binding.root) {
+    fun bind(loadState: LoadState) {
+        binding.claimsLoadingProgressBar.visibility
+    }
+}
