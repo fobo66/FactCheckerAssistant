62f0731f74104ecaa68a938f0682cb8fe3021a27
Andrey Mukamolov <fobo66@protonmail.com>
2020-07-27 21:29:24 +0300

    Cleaned up dependencies for better maintainability

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,12 +5,12 @@ buildscript {
     }
     dependencies {
         classpath("com.android.tools.build:gradle:4.2.0-alpha05")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:${Versions.kotlin}")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.3.72")
     }
 }
 
 plugins {
-    id("io.gitlab.arturbosch.detekt").version(Versions.detekt)
+    id("io.gitlab.arturbosch.detekt").version("1.10.0")
 }
 
 allprojects {
