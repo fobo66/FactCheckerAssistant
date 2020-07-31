81cd19f76bf28d99c7b88d51b9561f64177a6b83
Andrey Mukamolov <amv@bamboogroup.eu>
2020-07-31 10:05:59 +0300

    Updated AGP

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ buildscript {
         jcenter()
     }
     dependencies {
-        classpath("com.android.tools.build:gradle:4.2.0-alpha06")
+        classpath("com.android.tools.build:gradle:4.2.0-alpha07")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.3.72")
     }
 }
