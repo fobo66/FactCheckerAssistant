c9bea87bbbdbed963e5623d7add733184910987a
Andrey Mukamolov <fobo66@protonmail.com>
2020-10-01 20:44:45 +0300

    Add previewable Compose function for content

--- app/src/main/java/io/github/fobo66/factcheckerassistant/FactCheckGuideFragment.kt
+++ app/src/main/java/io/github/fobo66/factcheckerassistant/FactCheckGuideFragment.kt
@@ -5,9 +5,15 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.compose.foundation.Text
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Scaffold
+import androidx.compose.material.TopAppBar
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.platform.ComposeView
 import androidx.fragment.app.Fragment
+import androidx.ui.tooling.preview.Preview
 
 class FactCheckGuideFragment : Fragment() {
     override fun onCreateView(
@@ -18,11 +24,20 @@ class FactCheckGuideFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_fact_check_guide, container, false)
             .apply {
                 findViewById<ComposeView>(R.id.factCheckGuideRoot).setContent {
-                    // In Compose world
-                    MaterialTheme {
-                        Text("Hello Compose!")
-                    }
+                    FactCheckGuideContent()
                 }
             }
+    }
+
+    @Composable
+    @Preview
+    fun FactCheckGuideContent() = MaterialTheme {
+        Scaffold {
+            TopAppBar(
+                title = {
+                    Text("getString(R.string.fact_check_guide_title)")
+                }
+            )
+        }
     }
 }
