d343f7b849508392390bb0cc4e155756f60ac0dc
Andrey Mukamolov <amv@bamboogroup.eu>
2020-10-02 13:19:45 +0300

    Fixed lint failure

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -43,9 +43,17 @@ android {
     buildTypes {
         getByName("release") {
             isMinifyEnabled = true
-            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
         }
     }
+
+    lintOptions {
+        disable("InvalidFragmentVersionForActivityResult")
+    }
+
 }
 
 dependencies {
