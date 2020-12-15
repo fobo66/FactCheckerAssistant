47205c77a3cf90138747809b08b0efcf24507406
Andrey Mukamolov <fobo66@protonmail.com>
2020-12-16 00:36:04 +0300

    Updated AGP

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ buildscript {
         jcenter()
     }
     dependencies {
-        classpath("com.android.tools.build:gradle:7.0.0-alpha02")
+        classpath("com.android.tools.build:gradle:7.0.0-alpha03")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.4.21")
     }
 }
