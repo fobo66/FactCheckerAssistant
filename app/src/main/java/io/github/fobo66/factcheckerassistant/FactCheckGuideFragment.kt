8e5e804686698fafa96badf17a7f67a7ae1d7388
Andrey Mukamolov <amv@bamboogroup.eu>
2020-12-17 17:25:53 +0300

    Fixed Detekt complaints

--- app/src/main/java/io/github/fobo66/factcheckerassistant/FactCheckGuideFragment.kt
+++ app/src/main/java/io/github/fobo66/factcheckerassistant/FactCheckGuideFragment.kt
@@ -4,7 +4,13 @@ import android.os.Bundle
 import android.view.View
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.RowScope
-import androidx.compose.material.*
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Scaffold
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.material.TopAppBar
+import androidx.compose.material.contentColorFor
+import androidx.compose.material.primarySurface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
