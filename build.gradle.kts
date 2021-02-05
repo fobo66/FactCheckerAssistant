8cb74e81ddd59bbd3ce4933631ab50ec21b4463b
Andrey Mukamolov <amv@bamboogroup.eu>
2021-02-05 18:24:57 +0300

    Updated Kotlin

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ buildscript {
     }
     dependencies {
         classpath("com.android.tools.build:gradle:7.0.0-alpha05")
-        classpath(kotlin("gradle-plugin", version = "1.4.21-2"))
+        classpath(kotlin("gradle-plugin", version = "1.4.30"))
         classpath("com.google.dagger:hilt-android-gradle-plugin:2.31.2-alpha")
     }
 }
