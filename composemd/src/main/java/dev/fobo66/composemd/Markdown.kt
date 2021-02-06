61ea4acd9029cf9f816727a74a8d7d7b75399a3a
Andrey Mukamolov <fobo66@protonmail.com>
2021-02-06 14:39:12 +0300

    Experimental odule for rendering Markdown documents
    This module might be extracted to the separate project and published as a library
    on Maven Central, but for now I want ot test if it works.

---
+++ composemd/src/main/java/dev/fobo66/composemd/Markdown.kt
@@ -0,0 +1,245 @@
+package dev.fobo66.composemd
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.InlineTextContent
+import androidx.compose.foundation.text.appendInlineContent
+import androidx.compose.material.Colors
+import androidx.compose.material.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.gesture.tapGestureFilter
+import androidx.compose.ui.platform.AmbientUriHandler
+import androidx.compose.ui.text.AnnotatedString
+import androidx.compose.ui.text.Placeholder
+import androidx.compose.ui.text.PlaceholderVerticalAlign
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.TextLayoutResult
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextDecoration
+import androidx.compose.ui.unit.dp
+import dev.chrisbanes.accompanist.coil.CoilImage
+import org.commonmark.node.BlockQuote
+import org.commonmark.node.BulletList
+import org.commonmark.node.Code
+import org.commonmark.node.Document
+import org.commonmark.node.Emphasis
+import org.commonmark.node.FencedCodeBlock
+import org.commonmark.node.HardLineBreak
+import org.commonmark.node.Heading
+import org.commonmark.node.Image
+import org.commonmark.node.IndentedCodeBlock
+import org.commonmark.node.Link
+import org.commonmark.node.Node
+import org.commonmark.node.OrderedList
+import org.commonmark.node.Paragraph
+import org.commonmark.node.StrongEmphasis
+import org.commonmark.node.Text
+import org.commonmark.node.ThematicBreak
+
+const val TAG_URL = "TAG_URL"
+const val TAG_IMAGE_URL = "TAG_IMAGE_URL"
+
+/**
+ * Render Markdown document with Jetpack Compose.
+ * Based on [Markdown Composer](https://github.com/ErikHellman/MarkdownComposer)
+ */
+
+@Composable
+fun MarkdownDocument(document: Document) {
+    MarkdownBlockChildren(document)
+}
+
+@Composable
+fun MarkdownBlockChildren(parent: Node) {
+    var child = parent.firstChild
+    while (child != null) {
+        when (child) {
+            is BlockQuote -> MarkdownBlockQuote(child)
+            is ThematicBreak -> MarkdownThematicBreak(child)
+            is Heading -> MarkdownHeading(child)
+            is Paragraph -> MarkdownParagraph(child)
+            is FencedCodeBlock -> MarkdownFencedCodeBlock(child)
+            is IndentedCodeBlock -> MarkdownIndentedCodeBlock(child)
+            is Image -> MarkdownImage(child)
+            is BulletList -> MarkdownBulletList(child)
+            is OrderedList -> MarkdownOrderedList(child)
+        }
+        child = child.next
+    }
+}
+
+@Composable
+fun MarkdownOrderedList(child: OrderedList) {
+    TODO("Not yet implemented")
+}
+
+@Composable
+fun MarkdownBulletList(child: BulletList) {
+    TODO("Not yet implemented")
+}
+
+@Composable
+fun MarkdownImage(image: Image) {
+    Box(modifier = Modifier.fillMaxWidth(), contentAlignment = Alignment.Center) {
+        CoilImage(data = image.destination, contentDescription = image.title)
+    }
+}
+
+@Composable
+fun MarkdownIndentedCodeBlock(indentedCodeBlock: IndentedCodeBlock) {
+    TODO("Not yet implemented")
+}
+
+@Composable
+fun MarkdownFencedCodeBlock(child: FencedCodeBlock) {
+    TODO("Not yet implemented")
+}
+
+@Composable
+fun MarkdownParagraph(paragraph: Paragraph) {
+    if (paragraph.firstChild is Image && paragraph.firstChild == paragraph.lastChild) {
+        // Paragraph with single image
+        MarkdownImage(paragraph.firstChild as Image)
+    } else {
+        val padding = if (paragraph.parent is Document) 8.dp else 0.dp
+        Box(modifier = Modifier.padding(bottom = padding)) {
+            val styledText = buildAnnotatedString {
+                pushStyle(MaterialTheme.typography.body1.toSpanStyle())
+                appendMarkdownChildren(paragraph, MaterialTheme.colors)
+                pop()
+            }
+            MarkdownText(styledText, MaterialTheme.typography.body1)
+        }
+    }
+}
+
+@Composable
+fun MarkdownHeading(heading: Heading) {
+    val style = when (heading.level) {
+        1 -> MaterialTheme.typography.h1
+        2 -> MaterialTheme.typography.h2
+        3 -> MaterialTheme.typography.h3
+        4 -> MaterialTheme.typography.h4
+        5 -> MaterialTheme.typography.h5
+        6 -> MaterialTheme.typography.h6
+        else -> {
+            // Not a header...
+            MarkdownBlockChildren(heading)
+            return
+        }
+    }
+
+    val padding = if (heading.parent is Document) 8.dp else 0.dp
+    Box(modifier = Modifier.padding(bottom = padding)) {
+        val text = buildAnnotatedString {
+            appendMarkdownChildren(heading, MaterialTheme.colors)
+        }
+        MarkdownText(text, style)
+    }
+}
+
+@Composable
+fun MarkdownText(text: AnnotatedString, style: TextStyle) {
+    val uriHandler = AmbientUriHandler.current
+    val layoutResult = remember {
+        mutableStateOf<TextLayoutResult?>(null)
+    }
+
+    androidx.compose.material.Text(
+        text = text,
+        modifier = Modifier.tapGestureFilter { pos ->
+            layoutResult.value?.let {
+                val position = it.getOffsetForPosition(pos)
+                text.getStringAnnotations(position, position)
+                    .firstOrNull()
+                    ?.let { sa ->
+                        if (sa.tag == TAG_URL) {
+                            uriHandler.openUri(sa.item)
+                        }
+                    }
+            }
+        },
+        style = style,
+        inlineContent = mapOf(
+            TAG_IMAGE_URL to InlineTextContent(
+                Placeholder(
+                    style.fontSize, style.fontSize,
+                    PlaceholderVerticalAlign.Bottom
+                )
+            ) {
+                CoilImage(data = it, contentDescription = null)
+            }
+        ),
+        onTextLayout = { layoutResult.value = it }
+    )
+}
+
+@Composable
+fun MarkdownThematicBreak(child: ThematicBreak) {
+    TODO("Not yet implemented")
+}
+
+@Composable
+fun MarkdownBlockQuote(child: BlockQuote) {
+    TODO("Not yet implemented")
+}
+
+private fun AnnotatedString.Builder.appendMarkdownChildren(
+    parent: Node, colors: Colors
+) {
+    var child = parent.firstChild
+    while (child != null) {
+        when (child) {
+            is Paragraph -> appendMarkdownChildren(child, colors)
+            is Text -> append(child.literal)
+            is Image -> appendInlineContent(
+                TAG_IMAGE_URL,
+                child.destination
+            )
+            is Emphasis -> {
+                pushStyle(SpanStyle(fontStyle = FontStyle.Italic))
+                appendMarkdownChildren(child, colors)
+                pop()
+            }
+            is StrongEmphasis -> {
+                pushStyle(SpanStyle(fontWeight = FontWeight.Bold))
+                appendMarkdownChildren(child, colors)
+                pop()
+            }
+            is Code -> {
+                pushStyle(
+                    TextStyle(
+                        fontFamily =
+                        FontFamily.Monospace
+                    ).toSpanStyle()
+                )
+                append(child.literal)
+                pop()
+            }
+            is HardLineBreak -> {
+                append("\n")
+            }
+            is Link -> {
+                val underline = SpanStyle(
+                    colors.primary,
+                    textDecoration = TextDecoration.Underline
+                )
+                pushStyle(underline)
+                pushStringAnnotation(TAG_URL, child.destination)
+                appendMarkdownChildren(child, colors)
+                pop()
+                pop()
+            }
+        }
+        child = child.next
+    }
+}
