2c7fb6296fbc6ba97e17eb24950bc2947f9121cd
Andrey Mukamolov <amv@bamboogroup.eu>
2021-08-11 11:05:04 +0300

    Added draft theme

---
+++ app/src/main/java/io/github/fobo66/factcheckerassistant/ui/theme/Color.kt
@@ -0,0 +1,9 @@
+package io.github.fobo66.factcheckerassistant.ui.theme 
+
+import androidx.compose.ui.graphics.Color
+
+val Green500 = Color(0xff4caf50)
+val Green800 = Color(0xff087f22)
+val Green200 = Color(0xff80e27d)
+val Orange500 = Color(0xffff9800)
+val Orange200 = Color(0xffffc947)
