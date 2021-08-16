2ccca7bc90a4948a5e3ecedd22b54a555b685ff0
Andrey Mukamolov <fobo66@protonmail.com>
2021-08-16 19:58:19 +0300

    Added claim title to the details

--- app/src/main/java/io/github/fobo66/factcheckerassistant/ui/list/ClaimDetails.kt
+++ app/src/main/java/io/github/fobo66/factcheckerassistant/ui/list/ClaimDetails.kt
@@ -4,6 +4,7 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.ExperimentalMaterialApi
 import androidx.compose.material.ListItem
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
@@ -22,6 +23,11 @@ import io.github.fobo66.factcheckerassistant.ui.main.MainViewModel
 @Composable
 fun ClaimDetails(viewModel: MainViewModel = hiltViewModel()) {
     val claim by viewModel.selectedClaim.collectAsState()
+
+    Text(
+        text = claim?.text.orEmpty(),
+        style = MaterialTheme.typography.h3
+    )
 
     LazyColumn {
         items(claim?.claimReview ?: listOf()) {
