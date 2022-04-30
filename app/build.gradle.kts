e2644352851f0de2d20c5933ebbd8e6a4018c08a
Andrey Mukamolov <4471821+fobo66@users.noreply.github.com>
2022-05-01 00:51:12 +0300

    Fixed opt-in

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -62,7 +62,7 @@ android {
 }
 
 tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>().configureEach {
-    kotlinOptions.freeCompilerArgs += "-Xopt-in=kotlin.RequiresOptIn"
+    kotlinOptions.freeCompilerArgs += "-opt-in=kotlin.RequiresOptIn"
 }
 
 kapt {
