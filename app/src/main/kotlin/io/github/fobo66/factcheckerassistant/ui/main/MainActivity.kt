2d0db44349e72f5a104c226031087dad2994ed27
Andrey Mukamolov <4471821+fobo66@users.noreply.github.com>
2022-10-25 19:36:03 +0300

    Start testing new design changes

--- app/src/main/kotlin/io/github/fobo66/factcheckerassistant/ui/main/MainActivity.kt
+++ app/src/main/kotlin/io/github/fobo66/factcheckerassistant/ui/main/MainActivity.kt
@@ -6,13 +6,12 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
+import androidx.compose.material3.MediumTopAppBar
 import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
@@ -58,7 +57,7 @@ class MainActivity : ComponentActivity() {
                 val navController = rememberNavController()
                 Scaffold(
                     topBar = {
-                        TopAppBar(
+                        MediumTopAppBar(
                             title = {
                                 Text(
                                     stringResource(R.string.app_name)
