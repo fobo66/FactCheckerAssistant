19f28a12318d2dc6aa929cf29e246f505403ee5b
Andrey Mukamolov <4471821+fobo66@users.noreply.github.com>
2022-12-05 10:53:43 +0200

    Updated AGP

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ buildscript {
         mavenCentral()
     }
     dependencies {
-        classpath("com.android.tools.build:gradle:8.0.0-alpha08")
+        classpath("com.android.tools.build:gradle:8.0.0-alpha09")
         classpath(kotlin("gradle-plugin", version = "1.7.20"))
         classpath("com.google.dagger:hilt-android-gradle-plugin:2.44.2")
         classpath("io.gitlab.arturbosch.detekt:detekt-gradle-plugin:1.22.0")
