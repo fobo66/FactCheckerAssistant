4baf66018a42d39a7d617fb5b4977f435ed6a542
Andrey Mukamolov <4471821+fobo66@users.noreply.github.com>
2023-04-04 17:25:46 +0300

    Improved test

--- app/src/test/kotlin/io/github/fobo66/factcheckerassistant/util/LocalDateTimeAdapterTest.kt
+++ app/src/test/kotlin/io/github/fobo66/factcheckerassistant/util/LocalDateTimeAdapterTest.kt
@@ -2,7 +2,9 @@ package io.github.fobo66.factcheckerassistant.util
 
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
 import java.time.LocalDateTime
+import java.time.format.DateTimeParseException
 
 class LocalDateTimeAdapterTest {
     private val adapter = LocalDateTimeAdapter()
@@ -15,6 +17,13 @@ class LocalDateTimeAdapterTest {
     }
 
     @Test
+    fun `fail to serialize date`() {
+        assertThrows<DateTimeParseException> {
+            adapter.fromJson(WRONG_TIMESTAMP)
+        }
+    }
+
+    @Test
     fun `serialize date`() {
         val date = adapter.fromJson(EXPECTED_TIMESTAMP)
         assertEquals(now, date)
@@ -23,5 +32,6 @@ class LocalDateTimeAdapterTest {
     companion object {
         const val DEFAULT_TIMESTAMP = "2023-03-21T12:34:56.789"
         const val EXPECTED_TIMESTAMP = "2023-03-21T12:34:56.789Z"
+        const val WRONG_TIMESTAMP = "2023-03-21T12:34:56.789+01"
     }
 }
