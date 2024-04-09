a052bcce9321781e6d66a2bfb3a518bf217a6eb0
Andrei Mukamolau <4471821+fobo66@users.noreply.github.com>
2024-04-10 02:04:19 +0300

    Improved dependency resolution

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,16 +1,29 @@
 pluginManagement {
     repositories {
+        exclusiveContent {
+            forRepository { google() }
+            filter {
+                includeGroupAndSubgroups("androidx")
+                includeGroupAndSubgroups("com.android")
+                includeGroup("com.google.testing.platform")
+            }
+        }
         gradlePluginPortal()
         mavenCentral()
-        google()
     }
 }
 
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
+        google {
+            content {
+                includeGroupAndSubgroups("androidx")
+                includeGroupAndSubgroups("com.android")
+                includeGroupAndSubgroups("com.google")
+            }
+        }
         mavenCentral()
-        google()
     }
 }
 
