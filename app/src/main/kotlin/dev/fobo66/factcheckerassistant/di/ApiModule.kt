15810599748508723291b5ee115a8683bc4144ad
Andrei Mukamolau <4471821+fobo66@users.noreply.github.com>
2024-05-25 19:01:25 +0300

    Fixed lint complaints

--- app/src/main/kotlin/dev/fobo66/factcheckerassistant/di/ApiModule.kt
+++ app/src/main/kotlin/dev/fobo66/factcheckerassistant/di/ApiModule.kt
@@ -10,8 +10,8 @@ import io.ktor.client.HttpClient
 import io.ktor.client.engine.cio.CIO
 import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
 import io.ktor.serialization.kotlinx.json.json
-import kotlinx.serialization.json.Json
 import javax.inject.Singleton
+import kotlinx.serialization.json.Json
 
 @Module
 @InstallIn(SingletonComponent::class)
