3d3301aa1982397462f90352d0f3625ef3b66af0
Andrei Mukamolau <4471821+fobo66@users.noreply.github.com>
2024-07-11 18:56:26 +0300

    Fixed Detekt config

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,9 +17,17 @@ tasks {
     register("clean", Delete::class) {
         delete(rootProject.layout.buildDirectory)
     }
+}
 
-    withType<io.gitlab.arturbosch.detekt.Detekt> {
-        // Target version of the generated JVM bytecode. It is used for type resolution.
-        this.jvmTarget = "17"
+subprojects {
+    tasks {
+        withType<io.gitlab.arturbosch.detekt.Detekt>().configureEach {
+            // Target version of the generated JVM bytecode. It is used for type resolution.
+            jvmTarget = "17"
+        }
+        withType<io.gitlab.arturbosch.detekt.DetektCreateBaselineTask>().configureEach {
+            // Target version of the generated JVM bytecode. It is used for type resolution.
+            jvmTarget = "17"
+        }
     }
 }
