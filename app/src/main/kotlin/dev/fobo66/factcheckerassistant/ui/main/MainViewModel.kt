a4f50ef76fc84e54e5d871c91888cd642fe05cbf
Andrei Mukamolau <4471821+fobo66@users.noreply.github.com>
2026-04-28 23:28:11 +0300

    Fixed viewmodel

--- app/src/main/kotlin/dev/fobo66/factcheckerassistant/ui/main/MainViewModel.kt
+++ app/src/main/kotlin/dev/fobo66/factcheckerassistant/ui/main/MainViewModel.kt
@@ -1,8 +1,6 @@
 package dev.fobo66.factcheckerassistant.ui.main
 
-import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.asFlow
 import androidx.lifecycle.viewModelScope
 import androidx.paging.PagingData
 import dev.fobo66.factcheckerassistant.api.models.Claim
@@ -16,20 +14,24 @@ import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.FlowPreview
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.debounce
 import kotlinx.coroutines.flow.filterNot
 import kotlinx.coroutines.flow.flatMapLatest
 import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.flow.update
 
+private const val STATE_FLOW_TIMEOUT = 5000L
+private const val DEFAULT_PAGE_SIZE = 10
+private const val SEARCH_DEBOUNCE = 300
+
 @ViewModelKey
 @ContributesIntoMap(AppScope::class)
-class MainViewModel @Inject constructor(
-    private val factCheckRepository: FactCheckRepository,
-    private val handle: SavedStateHandle
-) : ViewModel() {
+class MainViewModel @Inject constructor(private val factCheckRepository: FactCheckRepository) :
+    ViewModel() {
 
     private val query = MutableStateFlow("")
+    private val _selectedClaim: MutableStateFlow<Claim?> = MutableStateFlow(null)
 
     @OptIn(FlowPreview::class, ExperimentalCoroutinesApi::class)
     val claims = query
@@ -40,26 +42,18 @@ class MainViewModel @Inject constructor(
         }
         .stateIn(
             viewModelScope,
-            SharingStarted.WhileSubscribed(5000),
+            SharingStarted.WhileSubscribed(STATE_FLOW_TIMEOUT),
             initialValue = PagingData.empty()
         )
 
-    val selectedClaim = handle.getLiveData<Claim>(KEY_SELECTED_CLAIM)
-        .asFlow()
-        .stateIn(viewModelScope, SharingStarted.Lazily, null)
+    val selectedClaim = _selectedClaim.asStateFlow()
+        .stateIn(viewModelScope, SharingStarted.WhileSubscribed(STATE_FLOW_TIMEOUT), null)
 
     fun search(query: String) {
         this.query.update { query }
     }
 
     fun selectClaim(claim: Claim?) {
-        handle.getLiveData<Claim>(KEY_SELECTED_CLAIM).postValue(claim)
-    }
-
-    companion object {
-        private const val KEY_QUERY = "query"
-        private const val KEY_SELECTED_CLAIM = "claim"
-        private const val DEFAULT_PAGE_SIZE = 10
-        private const val SEARCH_DEBOUNCE = 300
+        _selectedClaim.update { claim }
     }
 }
