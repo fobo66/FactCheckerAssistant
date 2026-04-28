1255e64ffc7e43c834ca947e596e454b455a5612
Andrei Mukamolau <4471821+fobo66@users.noreply.github.com>
2026-04-29 00:01:06 +0300

    Fixed detekt complaints

--- baselineprofile/src/main/java/dev/fobo66/baselineprofile/BaselineProfileGenerator.kt
+++ baselineprofile/src/main/java/dev/fobo66/baselineprofile/BaselineProfileGenerator.kt
@@ -7,25 +7,6 @@ import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 
-/**
- * This test class generates a basic startup baseline profile for the target package.
- *
- * We recommend you start with this but add important user flows to the profile to improve their performance.
- * Refer to the [baseline profile documentation](https://d.android.com/topic/performance/baselineprofiles)
- * for more information.
- *
- * You can run the generator with the Generate Baseline Profile run configuration,
- * or directly with `generateBaselineProfile` Gradle task:
- * ```
- * ./gradlew :Fact Checker Assistant.Fact_Checker_Assistant.app:generateReleaseBaselineProfile -Pandroid.testInstrumentationRunnerArguments.androidx.benchmark.enabledRules=BaselineProfile
- * ```
- * The run configuration runs the Gradle task and applies filtering to run only the generators.
- *
- * Check [documentation](https://d.android.com/topic/performance/benchmarking/macrobenchmark-instrumentation-args)
- * for more information about available instrumentation arguments.
- *
- * After you run the generator, you can verify the improvements running the [StartupBenchmarks] benchmark.
- **/
 @RunWith(AndroidJUnit4::class)
 @LargeTest
 class BaselineProfileGenerator {
